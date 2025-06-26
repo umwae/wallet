a23283f63e8c7d3491b03aaae1bd857eb76509af
Oleg Samkov <o.samkov@qsoft.ru>
2025-06-26 23:36:11 +0300

    Удалил дублирующий rest клиент

--- lib/src/core/rest_client/rest_client.dart
+++
@@ -1,7 +0,0 @@
-/// Rest client
-library;
-
-export 'src/exception/rest_client_exception.dart';
-export 'src/http/rest_client_http.dart';
-export 'src/rest_client.dart';
-export 'src/rest_client_base.dart';
