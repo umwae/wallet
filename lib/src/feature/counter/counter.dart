1c48d27c335e98b708f9eab5e3084837516ba340
Oleg Samkov <fixguard@gmail.com>
2025-10-16 00:08:12 +0300

    Удаление мусора

--- lib/src/feature/counter/counter.dart
+++
@@ -1,3 +0,0 @@
-//Add counter.dart to export all the public facing parts of the counter feature.
-export 'cubit/counter_cubit.dart';
-export 'view/view.dart';
--- lib/src/feature/counter/view/view.dart
+++
@@ -1,4 +0,0 @@
-//Barrel
-//Add view.dart to export all public facing parts of counter view.
-export 'counter_page.dart';
-export 'counter_view.dart';
